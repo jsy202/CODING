692d35c5ea8935ca08ee0bd68670a53b8281e81d
jsy202 <oie22230@gmail.com>
2026-09-07 00:17:05 +0900

    docs: add Day 4 stack and queue solutions

---
+++ PLAN/DAY4/01_스택.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    stack<int> st;
+
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; i++)
+    {
+        string s;
+        cin >> s;
+
+        if(s == "push")
+        {
+            int a;
+            cin >> a;
+            st.push(a);
+        }
+
+        else if(s == "pop")
+        {
+            if(st.empty())
+            {
+                cout << -1 << '\n';
+            }
+            else
+            {
+                cout << st.top() << '\n';
+                st.pop();
+            }
+        }
+
+        else if(s == "size")
+        {
+            cout << st.size() << '\n';
+        }
+
+        else if(s == "empty")
+        {
+            if(st.empty()) cout << 1 << '\n';
+            else cout << 0 << '\n';
+        }
+
+        else if(s == "top")
+        {
+            if(st.empty())
+            {
+                cout << -1 << '\n';
+            }
+            else
+            {
+                cout << st.top() << '\n';
+            }
+        }
+    }
+
+    return 0;
+}
